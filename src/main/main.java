1fc30ad9f39d51cb19ca68d9116a4fc3d684fd8f
thisbeto <albertohuber6475@gmail.com>
2025-03-12 19:09:07 -0300

    feature: Criação do formulário com file sendo criada e lida no console

---
+++ src/main/main.java
@@ -0,0 +1,10 @@
+package src.main;
+
+import src.repository.File;
+
+public class main {
+    public static void main(String[] args) {
+        File file = new File();
+        file.createFile();
+    }
+}
